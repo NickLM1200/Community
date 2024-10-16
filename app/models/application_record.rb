0f71babf44dca3f3a263439b027eaad15fe8728f
Nick LM <nledezma@msudenver.edu>
2024-10-16 17:19:06 -0600

    Login screen completed Username will always be lowercase (when created and signing in) Styles applied Show or hide password implemented

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  before_save :downcase_usernameemail
+
+  private
+
+  def downcase_usernameemail
+    self.username = username.downcase
+    self.email = email.downcase
+  end
 end
