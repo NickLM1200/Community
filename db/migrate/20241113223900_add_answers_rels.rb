1acb5df60f15873019bc21095985bae37a4a6bb1
Nick LM <nledezma@msudenver.edu>
2024-11-19 15:52:17 -0700

    migration cleaning

--- db/migrate/20241113223900_add_answers_rels.rb
+++ db/migrate/20241113223900_add_answers_rels.rb
@@ -2,7 +2,7 @@ class AddAnswersToSchema < ActiveRecord::Migration[7.1]
   def change
       create_table :question_answer_rels, primary_key: :qaID do |t| 
         t.references :questionID, foreign_key: {to_table: :questions, primary_key: :questionID}, type: :integer
-        t.references :answerID, foreign_key: {to_table: :answers, primary_key: :answerID}, type: :integer
+        t.string :admin_username
         t.timestamps
     end
   end
--- db/migrate/20241114221223_rename_survey_i_dto_id.rb
+++
@@ -1,5 +0,0 @@
-class RenameSurveyIDtoId < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :surveys, :surveyID, :id
-  end
-end
