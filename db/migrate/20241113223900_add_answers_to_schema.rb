dc6b687d65ab8edd3ebc51e2f9bf32b523f87c69
Nick LM <nledezma@msudenver.edu>
2024-11-20 12:23:44 -0700

    migration fixed

--- db/migrate/20241113223900_add_answers_to_schema.rb
+++ db/migrate/20241113223900_add_answers_to_schema.rb
@@ -1,7 +1,6 @@
 class AddAnswersToSchema < ActiveRecord::Migration[7.1]
   def change
       create_table :question_answer_rels, primary_key: :qaID do |t| 
-        t.references :questionID, foreign_key: {to_table: :questions, primary_key: :questionID}, type: :integer
         t.string :admin_username, null: true
         t.timestamps
     end
